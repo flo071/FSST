f0509f9d1a4ed9bb9a60d5323dc704b60875beed
flo071 <floh071@gmail.com>
2018-12-17 16:39:38 +0100

    update Lehrer.java

--- Lehrer.java
+++ Lehrer.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+
 public class Lehrer extends Mitarbeiter{
 	private String kuerzel;
+
+	public Lehrer(long svnr, String vorname, String nachname, LocalDate geburtsdatumm, String email, int anzahl, String kuerzel){
+		super(svnr, vorname, nachname, geburtsdatumm, email, anzahl);
+
+		this.kuerzel = kuerzel;
+	}
 
 	public String getKuerzel() {
 		return kuerzel;
