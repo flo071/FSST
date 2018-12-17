ac0b1575fd0861bd07a0a54713c5dd13af845722
flo071 <floh071@gmail.com>
2018-12-17 16:35:44 +0100

    update Mitarbeiter.java

--- Mitarbeiter.java
+++ Mitarbeiter.java
@@ -1,5 +1,14 @@
+import java.time.LocalDate;
+
 public class Mitarbeiter extends Person {
 	private int anzahl;
+
+	public Mitarbeiter(long svnr, String vorname, String nachname, LocalDate geburtsdatumm, String email, int anzahl)
+	{
+		super(svnr,vorname,nachname,geburtsdatumm,email);
+
+		this.anzahl = anzahl;
+	}
 
 	public int getAnzahl() {
 		return anzahl;
