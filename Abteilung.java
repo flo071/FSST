e2f65233f2a2160f967c04ae879b29aae0616b89
flo071 <floh071@gmail.com>
2019-01-15 11:12:52 +0100

    update Abteilung.java

--- Abteilung.java
+++ Abteilung.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+
 public class Abteilung {
-	public static void main(String[] args) {
-		//TODO: Start some method here
-		//flo was here
+	private String name, kuerzel;
+
+	private Lehrer abteilungsvorstand;
+	private Schueler abteilingssprecher;
+	private Schule schule;
+	private ArrayList<Lehrer> lehrer;
+	private ArrayList<Klasse> klassen;
+
+	public Abteilung(String name, String kuerzel, Schule schule) {
+		this.schule = schule;
+		this.name = name;
+		this.kuerzel = kuerzel;
+		lehrer = new ArrayList<Lehrer>();
+		klassen = new ArrayList<Klasse>();
+		abteilingssprecher = null;
+		abteilungsvorstand = null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getKuerzel() {
+		return kuerzel;
 	}
 }
