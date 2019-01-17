e2294d043e8e35ea8f7098a9a47f1898e81c7007
flo071 <floh071@gmail.com>
2019-01-17 16:03:43 +0100

    update Raum

--- Raum.java
+++ Raum.java
@@ -1,5 +1,24 @@
 public class Raum {
-	public static void main(String[] args) {
-		//TODO: Start some method here
-	}
+	private String raumNummer;
+	private int maxSitzplaetze;
+	private Raumtyp raumtyp;
+
+	public Raum(String raumNummer, int maxSitplaetze, Raumtyp raumtyp) {
+        this.raumNummer = raumNum;
+        this.maxSitzplaetze = maxSitpl;
+        this.raumtyp = raumtyp;
+    }
+
+    public String getRaumNummer() {
+        return raumNummer;
+    }
+
+    public int getMaxSitzplaetze() {
+        return maxSitzplaetze;
+    }
+
+    public Raumtyp getRaumtyp() {
+        return raumtyp;
+    }
+
 }
