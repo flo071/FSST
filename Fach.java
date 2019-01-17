55124d0f84f82dfdc4978e042345964d6d12cb3e
flo071 <floh071@gmail.com>
2019-01-17 16:08:48 +0100

    update Fach

--- Fach.java
+++ Fach.java
@@ -1,5 +1,23 @@
 public class Fach {
-	public static void main(String[] args) {
-		//TODO: Start some method here
-	}
+	private String name;
+    private int wochenstunden;
+    private Raumtyp raumanforderung;
+
+    public Fach(String name, int wochenstunden, Raumtyp raumanforderung){
+        this.name = name;
+        this.wochenstunden = wochenstunden;
+        this.raumanforderung = raumanforderung;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWochenstunden() {
+        return wochenstunden;
+    }
+
+    public Raumtyp getRaumanforderung() {
+        return raumanforderung;
+    }
 }
