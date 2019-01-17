8cd4aaee3a3c07655c35f6ab4deb2fb5a6a9626b
flo071 <floh071@gmail.com>
2019-01-17 23:31:44 +0100

    update

--- src/Klasse.java
+++ src/Klasse.java
@@ -14,4 +14,29 @@ public class Klasse {
 	public int getSchulstufe(){
 		return schulstufe;
 	}
+
+	public float getDurchschnittsalter() {
+        int alterAlle = 0;
+        int anzahl = 0;
+        LocalDate heute = LocalDate.now();
+
+        for (Schueler s : schueler) {
+            LocalDate geb = s.getGeburtsdatum();
+            alterAlle += Period.between(geb, heute).getYears();
+            anzahl++;
+        }
+
+        float durchschnitt = alterAlle/anzahl;
+
+        return durchschnitt;
+    }
+
+    public boolean setKlassensprecher(Schueler schueler) {
+        klassensprecher = schueler;
+        return true;
+    }
+
+    public boolean addSchueler(Schueler schueler) {
+        return schueler.add(schueler);
+    }
 }
