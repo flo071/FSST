b9fe13a90b19c8c66cd50a906ad109f1a47d1aa2
flo071 <floh071@gmail.com>
2019-01-17 16:03:21 +0100

    update Adresse

--- Adresse.java
+++ Adresse.java
@@ -1,5 +1,32 @@
 public class Adresse {
-	public static void main(String[] args) {
-		//TODO: Start some method here
+	private String ort, strasse;
+	private int plz, hausnummer;
+
+	public Adresse(String strasse, int hausnummer, int plz, String ort){
+        this.ort = ort;
+        this.strasse = strasse;
+        this.hausnummer = hausnummer;
+        this.plz = plz;
+    }
+
+	public String getOrt() {
+		return ort;
+	}
+
+	public String getStrasse(){
+		return strasse;
+	}
+
+	public int getHausnummer(){
+		return hausnummer;
+	}
+
+	public int getPlz(){
+		return plz;
+	}
+
+	public void printAdresse(){
+		System.out.println(getStrasse + " " + getHausnummer + "\n" + getPlz + " " + getOrt);
+
 	}
 }
